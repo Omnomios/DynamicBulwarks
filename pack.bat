721aa18d37b9e8611858cf9840d78d86d959c159
Stefan Kent <dev@stefan-kent.com>
2018-02-26 01:30:56 +1100

    Less output.

--- pack.bat
+++ pack.bat
@@ -1,10 +1,9 @@
-rem ## Packs mission into PBO files for distribution ##
-
 @echo off
+rem ## Packs mission into PBO files for distribution ##
 
 for %%* in (.) do set DirName=%%~nx*
 cd ..
-robocopy %DirName% packsource /s /e /XD .git /XF mapList.txt pack.bat /NJH /NJS
+robocopy %DirName% packsource /s /e /XD .git /XF mapList.txt pack.bat /NJH /NJS /NDL /NFL
 
 for /F "eol=; tokens=1 delims=," %%i in  ( ./%DirName%/mapList.txt ) do  (
    echo Processing %%i...
